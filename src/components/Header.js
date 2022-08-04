f3c8f7e841d5079da03f30afee78985a9425ebb8
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-08-04 14:25:59 +0500

    header designed, working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-import { Button, Segment } from 'semantic-ui-react'
+import { Header, Segment, Image } from 'semantic-ui-react'
 
 export const HeaderComponent=() =>{
     return(
     <div>
-        <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
+        <Segment basic inverted padded='very' vertical>
+          <Header as='h1'>Header</Header>
+          <Image size='small' centered src={'logo512.png'}/>
+          <Header as='h3'>Subheading Header</Header>
+          </Segment>
   </div>
     )
 }
