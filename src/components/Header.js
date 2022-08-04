d3e2eaa2bbb383e10c299432bd0064e428e74e35
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-08-04 14:11:58 +0500

    react semantic ui errors resolved now working fine

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { Button, Segment } from 'semantic-ui-react'
+
+export const HeaderComponent=() =>{
+    return(
+    <div>
+        <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
+  </div>
+    )
+}
